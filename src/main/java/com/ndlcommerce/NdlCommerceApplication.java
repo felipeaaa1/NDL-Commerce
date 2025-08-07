7b109be26d98f6adddb8d1f9548cb3fb66ff7497
Felipe Alves <felipe.arnaud@aluno.uece.br>
2025-08-07 20:28:14 -0300

    :art: style: add Spotless plugin to enforce code formatting

--- src/main/java/com/ndlcommerce/NdlCommerceApplication.java
+++ src/main/java/com/ndlcommerce/NdlCommerceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NdlCommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NdlCommerceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(NdlCommerceApplication.class, args);
+  }
 }
