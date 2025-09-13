0d62480f05bce0c2e8710c0e12f3ad8c034491e8
Felipe Alves <felipe.arnaud@aluno.uece.br>
2025-09-13 18:39:30 -0300

    🔧 chore(migration): improve V1__Initial_setup.sql and refine application.yml config

--- src/main/resources/db/migration/V1__Initial_setup.sql
+++ src/main/resources/db/migration/V1__Initial_setup.sql
@@ -1,113 +1,184 @@
--- V1__create_schema.sql
-
 CREATE SCHEMA IF NOT EXISTS ecommerce;
 SET search_path TO ecommerce;
 
--- User types
-CREATE TABLE user_type (
-    id UUID PRIMARY KEY,
-    name VARCHAR(50) UNIQUE NOT NULL
-);
+-- user_type
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'ecommerce' AND table_name = 'user_type') THEN
+        RAISE NOTICE 'Já existe a tabela user_type';
+    ELSE
+        RAISE NOTICE 'Criando tabela user_type';
+        CREATE TABLE ecommerce.user_type (
+            id UUID PRIMARY KEY,
+            name VARCHAR(50) UNIQUE NOT NULL
+        );
+    END IF;
+END $$;
 
--- System users
-CREATE TABLE app_user (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    user_type_id UUID REFERENCES user_type(id),
-    created_by UUID REFERENCES app_user(id),
-    created_at TIMESTAMP DEFAU2LT CURRENT_TIMESTAMP,
-    updated_by UUID REFERENCES app_user(id),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- app_user
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'ecommerce' AND table_name = 'app_user') THEN
+        RAISE NOTICE 'Já existe a tabela app_user';
+    ELSE
+        RAISE NOTICE 'Criando tabela app_user';
+        CREATE TABLE ecommerce.app_user (
+            id UUID PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password VARCHAR(255) NOT NULL,
+            user_type_id UUID REFERENCES ecommerce.user_type(id),
+            created_by UUID REFERENCES ecommerce.app_user(id),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_by UUID REFERENCES ecommerce.app_user(id),
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    END IF;
+END $$;
 
--- Customers
-CREATE TABLE customer (
-    id UUID PRIMARY KEY,
-    user_id UUID REFERENCES app_user(id) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    contact VARCHAR(50),
-    address TEXT,
-    active BOOLEAN DEFAULT TRUE,
-    created_by UUID REFERENCES app_user(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_by UUID REFERENCES app_user(id),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- customer
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'ecommerce' AND table_name = 'customer') THEN
+        RAISE NOTICE 'Já existe a tabela customer';
+    ELSE
+        RAISE NOTICE 'Criando tabela customer';
+        CREATE TABLE ecommerce.customer (
+            id UUID PRIMARY KEY,
+            user_id UUID REFERENCES ecommerce.app_user(id) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            contact VARCHAR(50),
+            address TEXT,
+            active BOOLEAN DEFAULT TRUE,
+            created_by UUID REFERENCES ecommerce.app_user(id),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_by UUID REFERENCES ecommerce.app_user(id),
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    END IF;
+END $$;
 
--- Products
-CREATE TABLE product (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    created_by UUID REFERENCES app_user(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_by UUID REFERENCES app_user(id),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- product
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'ecommerce' AND table_name = 'product') THEN
+        RAISE NOTICE 'Já existe a tabela product';
+    ELSE
+        RAISE NOTICE 'Criando tabela product';
+        CREATE TABLE ecommerce.product (
+            id UUID PRIMARY KEY,
+            name VARCHAR(255) NOT NULL,
+            description TEXT,
+            created_by UUID REFERENCES ecommerce.app_user(id),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_by UUID REFERENCES ecommerce.app_user(id),
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    END IF;
+END $$;
 
--- Product variations (SKUs)
-CREATE TABLE product_sku (
-    id UUID PRIMARY KEY,
-    product_id UUID REFERENCES product(id) NOT NULL,
-    attributes JSONB,
-    price DECIMAL(10,2) NOT NULL,
-    stock INT NOT NULL
-);
+-- product_sku
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'ecommerce' AND table_name = 'product_sku') THEN
+        RAISE NOTICE 'Já existe a tabela product_sku';
+    ELSE
+        RAISE NOTICE 'Criando tabela product_sku';
+        CREATE TABLE ecommerce.product_sku (
+            id UUID PRIMARY KEY,
+            product_id UUID REFERENCES ecommerce.product(id) NOT NULL,
+            attributes JSONB,
+            price DECIMAL(10,2) NOT NULL,
+            stock INT NOT NULL
+        );
+    END IF;
+END $$;
 
--- Orders
-CREATE TABLE order_header (
-    id UUID PRIMARY KEY,
-    customer_id UUID REFERENCES customer(id),
-    status VARCHAR(30) NOT NULL CHECK (status IN (
-        'waiting_payment',
-        'paid',
-        'payment_denied',
-        'processing',
-        'shipped',
-        'delivered'
-    )),
-    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    total DECIMAL(10,2),
-    created_by UUID REFERENCES app_user(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_by UUID REFERENCES app_user(id),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- order_header
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'ecommerce' AND table_name = 'order_header') THEN
+        RAISE NOTICE 'Já existe a tabela order_header';
+    ELSE
+        RAISE NOTICE 'Criando tabela order_header';
+        CREATE TABLE ecommerce.order_header (
+            id UUID PRIMARY KEY,
+            customer_id UUID REFERENCES ecommerce.customer(id),
+            status VARCHAR(30) NOT NULL CHECK (status IN (
+                'waiting_payment',
+                'paid',
+                'payment_denied',
+                'processing',
+                'shipped',
+                'delivered'
+            )),
+            order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            total DECIMAL(10,2),
+            created_by UUID REFERENCES ecommerce.app_user(id),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_by UUID REFERENCES ecommerce.app_user(id),
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    END IF;
+END $$;
 
--- Order items
-CREATE TABLE order_item (
-    id UUID PRIMARY KEY,
-    order_id UUID NOT NULL REFERENCES order_header(id),
-    sku_id UUID NOT NULL REFERENCES product_sku(id),
-    quantity INT NOT NULL CHECK (quantity > 0),
-    unit_price DECIMAL(10,2) NOT NULL,
-    subtotal DECIMAL(10,2) NOT NULL,
-    created_by UUID REFERENCES app_user(id),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_by UUID REFERENCES app_user(id),
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- order_item
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'ecommerce' AND table_name = 'order_item') THEN
+        RAISE NOTICE 'Já existe a tabela order_item';
+    ELSE
+        RAISE NOTICE 'Criando tabela order_item';
+        CREATE TABLE ecommerce.order_item (
+            id UUID PRIMARY KEY,
+            order_id UUID NOT NULL REFERENCES ecommerce.order_header(id),
+            sku_id UUID NOT NULL REFERENCES ecommerce.product_sku(id),
+            quantity INT NOT NULL CHECK (quantity > 0),
+            unit_price DECIMAL(10,2) NOT NULL,
+            subtotal DECIMAL(10,2) NOT NULL,
+            created_by UUID REFERENCES ecommerce.app_user(id),
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_by UUID REFERENCES ecommerce.app_user(id),
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    END IF;
+END $$;
 
--- Cart
-CREATE TABLE cart (
-    id UUID PRIMARY KEY,
-    customer_id UUID REFERENCES customer(id),
-    total DECIMAL(10,2) DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+-- cart
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'ecommerce' AND table_name = 'cart') THEN
+        RAISE NOTICE 'Já existe a tabela cart';
+    ELSE
+        RAISE NOTICE 'Criando tabela cart';
+        CREATE TABLE ecommerce.cart (
+            id UUID PRIMARY KEY,
+            customer_id UUID REFERENCES ecommerce.customer(id),
+            total DECIMAL(10,2) DEFAULT 0,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    END IF;
+END $$;
 
-CREATE TABLE cart_item (
-    id UUID PRIMARY KEY,
-    cart_id UUID REFERENCES cart(id),
-    sku_id UUID REFERENCES product_sku(id),
-    quantity INT NOT NULL,
-    price DECIMAL(10,2) NOT NULL
-);
+-- cart_item
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.tables WHERE table_schema = 'ecommerce' AND table_name = 'cart_item') THEN
+        RAISE NOTICE 'Já existe a tabela cart_item';
+    ELSE
+        RAISE NOTICE 'Criando tabela cart_item';
+        CREATE TABLE ecommerce.cart_item (
+            id UUID PRIMARY KEY,
+            cart_id UUID REFERENCES ecommerce.cart(id),
+            sku_id UUID REFERENCES ecommerce.product_sku(id),
+            quantity INT NOT NULL,
+            price DECIMAL(10,2) NOT NULL
+        );
+    END IF;
+END $$;
 
 -- Sales report view
-CREATE OR REPLACE VIEW sales_report AS
+CREATE OR REPLACE VIEW ecommerce.sales_report AS
 SELECT
     o.id AS order_id,
     DATE_TRUNC('day', o.order_date) AS period,
@@ -119,18 +190,18 @@ SELECT
     SUM(oi.quantity) AS units_per_product,
     SUM(oi.subtotal) AS revenue_per_product
 FROM
-    order_header o
+    ecommerce.order_header o
 JOIN
-    order_item oi ON o.id = oi.order_id
+    ecommerce.order_item oi ON o.id = oi.order_id
 JOIN
-    product_sku s ON oi.sku_id = s.id
+    ecommerce.product_sku s ON oi.sku_id = s.id
 JOIN
-    product p ON s.product_id = p.id
+    ecommerce.product p ON s.product_id = p.id
 GROUP BY
     o.id, period, p.id, p.name, o.status;
 
 -- Indexes
-CREATE INDEX idx_app_user_email ON app_user(email);
-CREATE INDEX idx_order_customer ON order_header(customer_id);
-CREATE INDEX idx_sku_product ON product_sku(product_id);
-CREATE INDEX idx_order_item_sku ON order_item(sku_id);
+CREATE INDEX IF NOT EXISTS idx_app_user_email ON ecommerce.app_user(email);
+CREATE INDEX IF NOT EXISTS idx_order_customer ON ecommerce.order_header(customer_id);
+CREATE INDEX IF NOT EXISTS idx_sku_product ON ecommerce.product_sku(product_id);
+CREATE INDEX IF NOT EXISTS idx_order_item_sku ON ecommerce.order_item(sku_id);
