d4b38095ef55ac28457b62dd89629cde97788793
felipeaaa1 <75031055+felipeaaa1@users.noreply.github.com>
2025-12-08 10:03:30 -0300

    :construction_worker: chore(ci): add GitHub Actions workflow (build, test, upload artifacts) (#10)

--- src/test/java/com/ndlcommerce/NdlCommerceApplicationTests.java
+++ src/test/java/com/ndlcommerce/NdlCommerceApplicationTests.java
@@ -2,8 +2,10 @@ package com.ndlcommerce;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = {NdlCommerceApplication.class, TestConfig.class})
+@ActiveProfiles("test")
 class NdlCommerceApplicationTests {
 
   @Test
