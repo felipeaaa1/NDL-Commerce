e5ccb2a4e8fc8090743181ec8195f3062a496eb8
Felipe Alves <felipe.arnaud@aluno.uece.br>
2026-01-09 22:38:23 -0300

    feat(Customer): implement update customer

--- src/main/java/com/ndlcommerce/adapters/presenter/CustomerResponseFormatter.java
+++ src/main/java/com/ndlcommerce/adapters/presenter/CustomerResponseFormatter.java
@@ -1,12 +1,12 @@
 package com.ndlcommerce.adapters.presenter;
 
 import com.ndlcommerce.exception.BusinessException;
+import com.ndlcommerce.exception.EntityAlreadyExistsException;
 import com.ndlcommerce.useCase.interfaces.customer.CustomerPresenter;
 import com.ndlcommerce.useCase.request.customer.CustomerResponseDTO;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CustomerResponseFormatter implements CustomerPresenter {
 
@@ -15,7 +15,8 @@ public class CustomerResponseFormatter implements CustomerPresenter {
           "UserNotExistsOrInactive",
               new BusinessException("Usuário não encontrado ou esta inativo"),
               "NameNotValid", new BusinessException("Nome do Cliente não é válido. Nome deve ter entre 4 e 50 caracteres."),
-              "existsByName", new BusinessException("Nome de Cliente já registrado."));
+              "existsByName", new EntityAlreadyExistsException("Nome de Cliente já registrado."),
+              "NotFound", new NoSuchElementException());
 
   @Override
   public CustomerResponseDTO prepareSuccessView(CustomerResponseDTO customer) {
@@ -39,6 +40,13 @@ public class CustomerResponseFormatter implements CustomerPresenter {
 
   @Override
   public List<CustomerResponseDTO> prepareListSuccessView(List<CustomerResponseDTO> list) {
+    list.forEach(customer ->{
+              LocalDateTime customerDateTime = LocalDateTime.parse(customer.getCreationTime());
+              customer.setCreationTime(
+                      customerDateTime.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm")));
+
+            }
+            );
     return list;
   }
 }
